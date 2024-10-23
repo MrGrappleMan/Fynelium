1a5e5987c844ff5bd7046c4d253139c0a986f79a
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2024-10-23 16:48:05 +0530

    Permanent Rawhide
    Networking is the issue ig

--- main.fish
+++ main.fish
@@ -8,33 +8,33 @@ cp -r LXroot/etc/* /etc/
 # Repo Management:
 rpm-ostree cancel -q
 rpm-ostree reload -q
-#set base (rpm-ostree status | grep '● ' | awk '{print $2}')
-#if echo $base | grep -q "bazzite"
-#	set base (echo $base | sed 's/stable/unstable/g; s/testing/unstable/g')
-#  rpm-ostree rebase "$base" --experimental
-#end
-#set base (rpm-ostree status | grep '● ' | awk '{print $2}')
-#if echo $base | grep -q 'fedora:fedora/' -o -q '/silverblue' -o -q '/kinoite'
-#	rpm-ostree rebase fedora:fedora/rawhide/x86_64/silverblue --experimental
-#end
-#rpm-ostree reload -q
+set base (rpm-ostree status | grep '● ' | awk '{print $2}')
+if echo $base | grep -q "bazzite"
+set base (echo $base | sed 's/stable/unstable/g; s/testing/unstable/g')
+rpm-ostree rebase "$base" --experimental
+end
+set base (rpm-ostree status | grep '● ' | awk '{print $2}')
+if echo $base | grep -q 'fedora:fedora/' -o -q '/silverblue' -o -q '/kinoite'
+rpm-ostree rebase fedora:fedora/rawhide/x86_64/silverblue --experimental
+end
+rpm-ostree reload -q
 rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-rawhide.noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-rawhide.noarch.rpm
 rpm-ostree upgrade --allow-downgrade -q
 # Packages:
 # System Background Services
-rpm-ostree install tlp tlp-rdw
-rpm-ostree install boinc-client
-rpm-ostree install tor
+#rpm-ostree install tlp tlp-rdw
+#rpm-ostree install boinc-client
+#rpm-ostree install tor
 # User Applications
-rpm-ostree install boinc-manager
+#rpm-ostree install boinc-manager
 # Apply for configuration in current session
 rpm-ostree apply-live --allow-replacement
 
 # SystemD Services:
-systemctl enable tor
-systemctl enable --now tlp
+#systemctl enable tor
+#systemctl enable --now tlp
 systemctl enable rpm-ostreed-automatic.service rpm-ostreed-automatic.timer
-systemctl enable --now systemd-resolved systemd-networkd
+#systemctl enable --now systemd-resolved systemd-networkd
 # systemctl mask systemd-rfkill.service systemd-rfkill.socket
 
 # Flatpak:-
