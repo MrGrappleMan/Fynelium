47f0f005aad9f2f5d1f51ad86ad3239343d8cafa
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2024-10-26 02:49:00 +0530

    Remove OMF

--- start.bash
+++ start.bash
@@ -10,5 +10,4 @@ rpm-ostree cancel -q
 rpm-ostree reload -q
 rpm-ostree install fish
 rpm-ostree apply-live --allow-replacement
-curl -s https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish -c "source; exit"
 sudo /bin/fish -c "./main.fish"
