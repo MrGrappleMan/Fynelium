8189f1c29a15b63264ae88aaffa7a24e888ec238
Avaneesh <90239357+MrGrappleMan@users.noreply.github.com>
2024-12-03 13:35:47 +0530

    Update main.fish

--- main.fish
+++ main.fish
@@ -32,65 +32,43 @@ end
 rqe install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-rawhide.noarch.rpm \
 	https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-rawhide.noarch.rpm
 # Packages:
-listedexec "tlp tlp-rdw
+listedexec "tlp
+tlp-rdw
 tor
-moby-engine
-gnome-terminal
-podman
-distrobox
-distcc-server" "rqe install --allow-inactive --idempotent \$crntval"
+cosmic-desktop
+cosmic-session
+sddm" "rqe install --allow-inactive --idempotent \$crntval"
 rqe uninstall --allow-inactive --idempotent \
 	power-profiles-daemon
 
 # Flatpak:-
 # Configuration:
-set flatpak_repos \
-	"flathub=https://dl.flathub.org/repo/flathub.flatpakrepo \
-	flathub-beta=https://flathub.org/beta-repo/flathub-beta.flatpakrepo \
-	gnome-nightly=https://nightly.gnome.org/gnome-nightly.flatpakrepo \
-	fedora=oci+https://registry.fedoraproject.org \
-	fedora-testing=oci+https://registry.fedoraproject.org/#testing \
-	rhel=https://flatpaks.redhat.io/rhel.flatpakrepo \
-	webkit-sdk=https://software.igalia.com/flatpak-refs/webkit-sdk.flatpakrepo \
-	eclipse-nightly=https://download.eclipse.org/linuxtools/flatpak-I-builds/eclipse.flatpakrepo \
-	xwaylandvideobridge-nightly=https://cdn.kde.org/flatpak/xwaylandvideobridge-nightly/xwaylandvideobridge-nightly.flatpakrepo"
-for repo in $flatpak_repos
-    set name (echo $repo | cut -d '=' -f 1)
-    set url (echo $repo | cut -d '=' -f 2)
-    flatpak remote-add --if-not-exists --system $name $url
-end
+listedexec "flathub=https://dl.flathub.org/repo/flathub.flatpakrepo
+flathub-beta=https://flathub.org/beta-repo/flathub-beta.flatpakrepo
+gnome-nightly=https://nightly.gnome.org/gnome-nightly.flatpakrepo
+fedora=oci+https://registry.fedoraproject.org
+fedora-testing=oci+https://registry.fedoraproject.org/#testing
+rhel=https://flatpaks.redhat.io/rhel.flatpakrepo
+webkit-sdk=https://software.igalia.com/flatpak-refs/webkit-sdk.flatpakrepo
+eclipse-nightly=https://download.eclipse.org/linuxtools/flatpak-I-builds/eclipse.flatpakrepo
+xwaylandvideobridge-nightly=https://cdn.kde.org/flatpak/xwaylandvideobridge-nightly/xwaylandvideobridge-nightly.flatpakrepo" "flatpak remote-add --if-not-exists --system \$crntval"
 flatpak update --noninteractive
 # Packages:
-set flatpak_pkgs \
-	"flathub=com.github.d4nj1.tlpui"
-for pkgstring in $flatpak_pkgs
-    set rrepo (echo $pkgstring | cut -d '=' -f 1)
-    set pkg (echo $pkgstring | cut -d '=' -f 2)
-    flatpak install --noninteractive --or-update $rrepo $pkg
-end
-
-# Docker:-
-# Containers:
-wget https://gitlab.torproject.org/tpo/anti-censorship/docker-snowflake-proxy/raw/main/docker-compose.yml
-docker compose up -d snowflake-proxy
-# Finalize:
-set CONTAINERS 'snowflake-proxy'
-docker run -d --name watchtower -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --cleanup --include-stopped --include-restarting --revive-stopped --interval 300 $CONTAINERS
-docker update --restart=always --memory-swap=-1 --cpus=0 --cpu-quota=0 --pids-limit=-1 --cpu-rt-period=2000000 (sudo docker ps -q -a)
+listedexec "flathub=com.github.d4nj1.tlpui" "flatpak install --noninteractive --or-update \$crntval"
 
 # System:-
 # Finalize Ostree pkgs:
-systemctl mask \
-	systemd-rfkill.service systemd-rfkill.socket
+listedexec "systemd-rfkill
+systemd-rfkill.socket
+gdm" "systemctl mask \$crntval"
 listedexec "tlp
 boinc-client
 docker
 rpm-ostreed-automatic
-rpm-ostreed-automatic.timer" "systemctl enable \$crntval"
-boinccmd --acct_mgr attach scienceunited.org 
+rpm-ostreed-automatic.timer
+sddm" "systemctl enable \$crntval"
 # Boot:
-plymouth-set-default-theme spinner
-rqe kargs --append-if-missing="rhgb,threadirqs,sysrq_always_enabled=0,consoleblank=0,quiet,loglevel=3,preempt=full"
+rqe kargs --append-if-missing="threadirqs,sysrq_always_enabled=0,consoleblank=1,quiet,loglevel=3,preempt=full"
 rqe initramfs --enable
 
 systemctl poweroff
