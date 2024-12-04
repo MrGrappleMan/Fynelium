9f43caf4c59dd1c03169ebb12c98b91006ac74fc
Avaneesh <90239357+MrGrappleMan@users.noreply.github.com>
2024-12-04 17:48:52 +0530

    Update start.bash

--- start.bash
+++ start.bash
@@ -4,10 +4,11 @@ rm -rf /tmp/Fynelium
 git clone https://github.com/MrGrappleMan/Fynelium.git /tmp/Fynelium
 chmod -R 755 /tmp/Fynelium
 cd /tmp/Fynelium
+alias rqe="rpm-ostree -q --peer"
 
 # Prepare for main script:
-rpm-ostree cancel -q
-rpm-ostree reload -q
-rpm-ostree install fish
-rpm-ostree apply-live --allow-replacement
+rqe cancel
+rqe reload
+rqe install fish
+rqe apply-live --allow-replacement
 sudo /bin/fish -c "./main.fish">/dev/null
