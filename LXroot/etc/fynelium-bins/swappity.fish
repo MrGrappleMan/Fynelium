fb22461238422ffd3d06acda0758723cbd4902d4
Avaneesh <90239357+MrGrappleMan@users.noreply.github.com>
2024-12-10 22:50:24 +0530

    Update swappity.fish

--- LXroot/etc/fynelium-bins/swappity.fish
+++ LXroot/etc/fynelium-bins/swappity.fish
@@ -2,41 +2,43 @@
 swapoff -a
 rmmod zram
 modprobe zram
+set tmem (awk '/^MemTotal:/ {print $2}' /proc/meminfo)
+echo $tmem | sudo tee /sys/block/zram0/disksize
+sudo mkswap /dev/zram0
+sudo swapon /dev/zram0
 
-function get_memory_used_percent
-    set mem_total (awk '/^MemTotal:/ {print $2}' /proc/meminfo)
-    set mem_free (awk '/^MemAvailable:/ {print $2}' /proc/meminfo)
-    echo (math "(1 - ($mem_free / $mem_total)) * 100")
+function mempercent
+    set fmem (awk '/^MemAvailable:/ {print $2}' /proc/meminfo)
+    echo (math "$fmem / $tmem * 100")
 end
 
 function scale_value
     set min $argv[1]
     set max $argv[2]
-    set percent (get_memory_used_percent)
+    set percent (mempercent)
     set range (math "$max - $min")
     echo (math "round($min + ($percent * $range / 100))")
 end
 
-function calculate_swappiness
+function calc_swappiness
     echo (scale_value 0 200)
 end
 
-function calculate_vfs_cache_pressure
+function calc_vfs_cache_pressure
     echo (scale_value 0 2147483647)
 end
 
-function calculate_huge_pages
-    echo (scale_value 128 102400)
+function calc_huge_pages
+    echo (scale_value 128 9999999999999999999)
 end
 
-function adjust_memory_settings
+function apply_args
     sysctl vm.swappiness=(calculate_swappiness)
     sysctl vm.vfs_cache_pressure=(calculate_vfs_cache_pressure)
     sysctl vm.nr_hugepages=(calculate_huge_pages)
 end
 
 while true
-    adjust_memory_settings
-    set delay (math "1 + (59 * (get_memory_used_percent / 100))")
-    sleep $delay
+    apply_args
+    sleep 60
 end
