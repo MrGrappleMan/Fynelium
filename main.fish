acc9b4b6a1f2ab5a7d820a1c41627570d40ddcf4
Avaneesh <90239357+MrGrappleMan@users.noreply.github.com>
2024-12-10 22:05:45 +0530

    Update main.fish

--- main.fish
+++ main.fish
@@ -17,19 +17,13 @@ chmod -R 755 /etc/
 cp -r LXroot/etc/* /etc/
 systemctl daemon-reload
 systemctl enable --now systemd-resolved systemd-networkd
-systemctl enable rpm-ostreed-automatic.service rpm-ostreed-automatic.timer
 systemctl stop rpm-ostreed-automatic.service rpm-ostreed-automatic.timer
 
 # RPM-OSTree:-
 # Configuration:
 rqe cancel
 rqe reload
-if echo $base | grep -q "bazzite"
-	set base (echo $base | sed 's/stable/unstable/g; s/testing/unstable/g')
-	rqe rebase --experimental "$base"
-end
-listedexec "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-rawhide.noarch.rpm
-https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-rawhide.noarch.rpm" "rqe install \$crntval"
+rqe install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-rawhide.noarch.rpm
 # Packages:
 listedexec "tlp
 tlp-rdw
@@ -37,7 +31,7 @@ tor
 cosmic-desktop
 cosmic-session
 sddm" "rqe install --allow-inactive --idempotent \$crntval"
-listedexec "power-profiles-daemon" "rqe install --allow-inactive --idempotent \$crntval"
+listedexec "power-profiles-daemon" "rqe remove --allow-inactive --idempotent \$crntval"
 
 # Flatpak:-
 # Configuration:
@@ -52,7 +46,7 @@ eclipse-nightly=https://download.eclipse.org/linuxtools/flatpak-I-builds/eclipse
 xwaylandvideobridge-nightly=https://cdn.kde.org/flatpak/xwaylandvideobridge-nightly/xwaylandvideobridge-nightly.flatpakrepo" "flatpak remote-add --if-not-exists --system \$crntval"
 flatpak update --noninteractive
 # Packages:
-listedexec "flathub=com.github.d4nj1.tlpui" "flatpak install --noninteractive --or-update \$crntval"
+listedexec "flathub com.github.d4nj1.tlpui" "flatpak install --noninteractive --or-update \$crntval"
 
 # System:-
 # Finalize Ostree pkgs:
