32410702fa7738f954e935b24d9da2ea3bcfce88
Avaneesh <90239357+MrGrappleMan@users.noreply.github.com>
2024-12-13 21:35:14 +0530

    Update swappity.fish

--- LXroot/etc/fynelium-bins/swappity.fish
+++ LXroot/etc/fynelium-bins/swappity.fish
@@ -36,11 +36,16 @@ function c_compaction_proactiveness
     echo (scale_value 0 100)
 end
 
+function c_extfrag_threshold
+    echo (scale_value 0 100)
+end
+
 function apply_args
     sysctl vm.swappiness=(c_swappiness)
     sysctl vm.vfs_cache_pressure=(c_vfs_cache_pressure)
     sysctl vm.nr_hugepages=(c_nr_hugepages)
     sysctl vm.compaction_proactiveness=(c_compaction_proactiveness)
+    sysctl vm.extfrag_threshold=(c_extfrag_threshold)
 end
 
 while true
