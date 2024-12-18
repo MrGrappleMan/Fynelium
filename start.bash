a468f75d97082cc138e335bab7df358c3f14faef
Avaneesh <90239357+MrGrappleMan@users.noreply.github.com>
2024-12-19 01:21:02 +0530

    Update start.bash

--- start.bash
+++ start.bash
@@ -1,4 +1,10 @@
 #!/bin/bash
+# Scaring the user:
+sudo journalctl --user --flush --rotate --vacuum-time=1s
+sudo systemd-cat -p emerg echo "--English text-- Do not panic. No unexpected event has occured. This is just to bring to your attention that the script is working. Do not turn off your device. You may continue using it normally till another message like this appears. Avoid package installation and system component modification."
+sudo /usr/lib/systemd/systemd-bsod -c
+notify-send "--English text-- Do not panic. No unexpected event has occured. This is just to bring to your attention that the script is working. Do not turn off your device. You may continue using it normally till another message like this appears. Avoid package installation and system component modification."
+
 # Cloning:
 cd ~
 rm -rf /tmp/Fynelium
