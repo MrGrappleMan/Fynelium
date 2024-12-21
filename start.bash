356a32825c99702bca7399b71a443d3f3d932ba2
Avaneesh <90239357+MrGrappleMan@users.noreply.github.com>
2024-12-21 08:48:13 +0530

    Update start.bash

--- start.bash
+++ start.bash
@@ -1,8 +1,4 @@
 #!/bin/bash
-# Execution verity:
-journalctl --user --flush --rotate --vacuum-time=1s
-bash -c 'systemd-cat -p emerg echo "$(printf %b \'\e[41m\' \'\e[8]\' \'\e[H\e[J\')English text for other vernacular language users. Do not panic. No unexpected event has occured. This is just to bring to your attention that the script is working. Do not turn off your device. You may continue using it normally till another message like this appears. Avoid package installation and system component modification. Press any key to exit this."'
-/usr/lib/systemd/systemd-bsod -c
 
 # Cloning:
 cd ~
@@ -15,9 +11,9 @@ cd /tmp/Fynelium
 systemctl stop rpm-ostreed-automatic rpm-ostreed-automatic.timer
 rpm-ostree -q --peer cancel
 rpm-ostree -q --peer reload
-rpm-ostree -q --peer upgrade # Fix dependancies left broken by goofy maintainance quality.
+rpm-ostree -q --peer upgrade # Fix dependancies
 rpm-ostree apply-live --allow-replacement
-rpm-ostree -q --peer --allow-inactive --idempotent install fish # This is arguably better. To those who cannot adjust to this modern script, grow up...
+rpm-ostree -q --peer --allow-inactive --idempotent install fish # Better script imo.
 rpm-ostree apply-live --allow-replacement
-echo You can exit now. Processin shifted to background.
+echo You can exit the terminal interface now. Processing shifted to background. 
 fish -c "./main.fish" >/dev/null 2>/tmp/Fynelium/errors.log
