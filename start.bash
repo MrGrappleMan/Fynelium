7f6b38b669d04bb988dbba9ebc27dd071a37cbca
Avaneesh <90239357+MrGrappleMan@users.noreply.github.com>
2024-12-25 00:12:57 +0530

    Update start.bash

--- start.bash
+++ start.bash
@@ -14,16 +14,3 @@ case "$uname" in
     (*) echo 'error: unsupported platform.'; exit 2; ;;
 esac;
 /bin/bash "$scriptFile";
-
-notify-send "🟡 Refyning..." "Avoid powering off. Use your session normally." -u critical
-systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target shutdown.target reboot.target poweroff.target halt.target
-
-# Prepare for main script:
-systemctl stop rpm-ostreed-automatic rpm-ostreed-automatic.timer
-rpm-ostree -q --peer cancel
-rpm-ostree -q --peer reload
-rpm-ostree -q --peer upgrade # Fix dependancies
-rpm-ostree apply-live --allow-replacement
-rpm-ostree -q --peer --allow-inactive --idempotent install fish # Better shell imo.
-rpm-ostree apply-live --allow-replacement
-fish -c "./main.fish"
