d115f24637115c3f35c086f5d1241e6f74dba1f2
Avaneesh <90239357+MrGrappleMan@users.noreply.github.com>
2024-12-29 10:59:45 +0530

    Update start.bash

--- start.bash
+++ start.bash
@@ -14,16 +14,12 @@ esac
 # Git Installation:
 rpm-ostree install git
 rpm-ostree apply-live --allow-replacement
+brew install git
 
 # Clone Repository:
-    cd ~ || exit 1
-    rm -rf /tmp/Fynelium
-    git clone https://github.com/MrGrappleMan/Fynelium.git /tmp/Fynelium || {
-        echo "Error: Failed to clone repository."
-        exit 1
-    }
-    chmod -R 755 /tmp/Fynelium
-    cd /tmp/Fynelium
-}
+rm -rf /tmp/Fynelium
+git clone https://github.com/MrGrappleMan/Fynelium.git /tmp/Fynelium
+chmod -R 755 /tmp/Fynelium
+cd /tmp/Fynelium
 
 /bin/bash $scriptFile
