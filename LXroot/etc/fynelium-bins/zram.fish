1c2f4539a552025fa99b461a2bcc825eb8c56159
Avaneesh <90239357+MrGrappleMan@users.noreply.github.com>
2025-01-05 04:16:03 +0530

    Update zram.fish

--- LXroot/etc/fynelium-bins/zram.fish
+++ LXroot/etc/fynelium-bins/zram.fish
@@ -4,6 +4,7 @@ rmmod zram
 modprobe zram
 set tmem (awk '/^MemTotal:/ {print $2}' /proc/meminfo)
 
+echo lzo > /sys/block/zram0/comp_algorithm
 echo $tmem | sudo tee /sys/block/zram0/disksize
 mkswap /dev/zram0
 swapon -p 10 /dev/zram0
