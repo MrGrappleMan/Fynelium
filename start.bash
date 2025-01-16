95480fe969311b5c27af94fc4365193480be4c61
Avaneesh <90239357+MrGrappleMan@users.noreply.github.com>
2025-01-16 14:19:54 +0530

    Update start.bash

--- start.bash
+++ start.bash
@@ -4,7 +4,17 @@
 rm -rf /tmp/Fynelium
 git clone https://github.com/MrGrappleMan/Fynelium.git /tmp/Fynelium
 chmod -R 755 /tmp/Fynelium
-cd /tmp/Fynelium || exit
+cd /tmp/Fynelium
 
-# Execute Script
-/bin/bash LX.bash
+# Prevent system power state change:
+systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target shutdown.target reboot.target poweroff.target halt.target
+
+# Prepare for main script:
+systemctl stop rpm-ostreed-automatic rpm-ostreed-automatic.timer
+rpm-ostree -q --peer cancel
+rpm-ostree -q --peer reload
+rpm-ostree -q --peer upgrade # Fix dependancies
+rpm-ostree apply-live --allow-replacement
+rpm-ostree -q --peer --allow-inactive --idempotent install fish dnf-repo
+rpm-ostree apply-live --allow-replacement
+fish /tmp/Fynelium/LXmain.fish
