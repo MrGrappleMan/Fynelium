15075d99fc1217b5de1bb6d0dea7b63564f99cdb
Avaneesh <90239357+MrGrappleMan@users.noreply.github.com>
2025-01-19 15:33:25 +0530

    Update rpm-ostree.fish

--- LXscripts/PKGREPO/rpm-ostree.fish
+++ LXscripts/PKGREPO/rpm-ostree.fish
@@ -1,1 +1,4 @@
 #!/bin/fish
+
+rpm-ostree -q --peer rebase fedora:fedora/rawhide/aarch64/silverblue
+rpm-ostree -q --peer install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-rawhide.noarch.rpm
