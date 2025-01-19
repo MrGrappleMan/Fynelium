b073e6adf8620b72389a188af02916f5440a0821
Avaneesh <90239357+MrGrappleMan@users.noreply.github.com>
2025-01-19 15:31:07 +0530

    Update flatpak.fish

--- LXscripts/PKGREPO/flatpak.fish
+++ LXscripts/PKGREPO/flatpak.fish
@@ -1,1 +1,17 @@
 #!/bin/fish
+source /etc/fish/config.fish
+
+listedexec "flatpak remote-add --if-not-exists --system \$crntval" "flathub https://flathub.org/repo/flathub.flatpakrepo
+flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
+gnome-nightly https://nightly.gnome.org/gnome-nightly.flatpakrepo
+webkit-sdk https://software.igalia.com/flatpak-refs/webkit-sdk.flatpakrepo
+fedora oci+https://registry.fedoraproject.org
+fedora-testing oci+https://registry.fedoraproject.org/#testing
+rhel https://flatpaks.redhat.io/rhel.flatpakrepo
+eclipse-nightly https://download.eclipse.org/linuxtools/flatpak-I-builds/eclipse.flatpakrepo
+elementaryos https://flatpak.elementary.io/repo.flatpakrepo
+pureos https://store.puri.sm/repo/stable/pureos.flatpakrepo
+kde-runtime-nightly https://cdn.kde.org/flatpak/kde-runtime-nightly/kde-runtime-nightly.flatpakrepo"
+
+listedexec "flatpak remote-modify --system --subset=floss \$crntval" "flathub
+flathub-beta" 
