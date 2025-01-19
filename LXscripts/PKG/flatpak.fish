aaa5c3e0d483d16e3505f894de7e33b2c6180f36
Avaneesh <90239357+MrGrappleMan@users.noreply.github.com>
2025-01-19 15:36:03 +0530

    Update flatpak.fish

--- LXscripts/PKG/flatpak.fish
+++ LXscripts/PKG/flatpak.fish
@@ -1,1 +1,8 @@
 #!/bin/fish
+source /etc/fish/config.fish
+
+listedexec "flatpak install --system --noninteractive --or-update \$crntval" "flathub com.gopeed.Gopeed
+flathub io.github.flattool.Warehouse
+flathub com.vscodium.codium-insiders
+flathub org.cubocore.CoreStats
+flathub org.octave.Octave"
