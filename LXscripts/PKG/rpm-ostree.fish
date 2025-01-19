04bab3a23ab73ae2b3b92695a6007300b5f60153
Avaneesh <90239357+MrGrappleMan@users.noreply.github.com>
2025-01-19 15:39:22 +0530

    Update rpm-ostree.fish

--- LXscripts/PKG/rpm-ostree.fish
+++ LXscripts/PKG/rpm-ostree.fish
@@ -1,1 +1,24 @@
 #!/bin/fish
+source /etc/fish/config.fish
+
+# RQE pkg+:
+listedexec "rpm-ostree -q --peer install --allow-inactive --idempotent \$crntval
+dnf install" "tlp tlp-rdw
+cosmic-desktop cosmic-session niri
+kernel-modules-extra grubby
+dnf dnf-repo
+ghostty
+rustup rust
+golang
+distcc
+ostree-devel
+zen-browser torbrowser-launcher
+boinc-client
+protontricks bottles
+topgrade gnome-software flatseal flatpak-selinux flatpak-session-helper xdg-desktop-portal flatpak-libs libportal host-spawn
+beep"
+
+# RQE pkg-:
+listedexec "rpm-ostree -q --peer uninstall --allow-inactive --idempotent \$crntval" "power-profiles-daemon
+firefox
+xwaylandvideobridge"
