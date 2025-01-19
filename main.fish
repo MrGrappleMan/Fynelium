44f9eca27eb21069a5602aee04a21ddf8ae0fd98
Avaneesh <90239357+MrGrappleMan@users.noreply.github.com>
2025-01-19 20:44:16 +0530

    Update main.fish

--- main.fish
+++ main.fish
@@ -1,21 +1,5 @@
 #!/bin/fish
 
-function rqe
-        rpm-ostree -q --peer $argv
-end
-
-function listedexec
-    set -l command_template $argv[1]
-    set -l contents $argv[2]
-
-    for subcommand in (echo "$command_template" | string split "\n")
-        for item in (echo "$contents" | string split "\n")
-            set -l command (string replace '$crntval' "$item" "$subcommand")
-            eval "$command"
-        end
-    end
-end
-
 # System:
 rm -rf /etc/yum.repos.d/*
 chmod -R 755 /etc/
