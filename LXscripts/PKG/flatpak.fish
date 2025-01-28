5b851c508617f8cbf63eb7aaa803e1d0e5f6da77
Avaneesh <90239357+MrGrappleMan@users.noreply.github.com>
2025-01-28 20:13:40 +0530

    Update flatpak.fish

--- LXscripts/PKG/flatpak.fish
+++ LXscripts/PKG/flatpak.fish
@@ -4,6 +4,10 @@ fyn-functions
 
 listedexec "flatpak install --system --noninteractive --or-update \$crntval" "flathub com.gopeed.Gopeed
 flathub io.github.flattool.Warehouse
+flathub com.github.rkoesters.xkcd-gtk
+flathub org.geogebra.GeoGebra
 flathub com.vscodium.codium-insiders
+flathub se.sjoerd.Graphs
 flathub org.cubocore.CoreStats
+flathub net.cozic.joplin_desktop
 flathub org.octave.Octave"
