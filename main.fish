6d05193a8637d6aab6fb8f5a4c580b283e1d6f74
Avaneesh <90239357+MrGrappleMan@users.noreply.github.com>
2025-03-10 19:13:53 +0530

    Update main.fish

--- main.fish
+++ main.fish
@@ -60,8 +60,8 @@ flatpak install --system --noninteractive --or-update flathub org.octave.Octave
 
 #RPM-OSTree
 	#Repos
-	rpm-ostree -q --peer rebase fedora:fedora/rawhide/aarch64/silverblue
-		rpm-ostree -q --peer install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-rawhide.noarch.rpm
+ rpm-ostree -q --peer rebase fedora:fedora/rawhide/aarch64/silverblue
+ rpm-ostree -q --peer install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-rawhide.noarch.rpm
 
 	#Pkgs
 rpm-ostree -q --peer install --allow-inactive --idempotent tlp tlp-rdw
@@ -80,25 +80,22 @@ rpm-ostree -q --peer install --allow-inactive --idempotent gnome-software
 rpm-ostree -q --peer install --allow-inactive --idempotent flatseal flatpak-selinux flatpak-session-helper xdg-desktop-portal flatpak-libs libportal host-spawn
 rpm-ostree -q --peer install --allow-inactive --idempotent beep
 
-listedexec "rpm-ostree -q --peer uninstall --allow-inactive --idempotent \$crntval" "power-profiles-daemon
-firefox
-xwaylandvideobridge"
+rpm-ostree -q --peer uninstall --allow-inactive --idempotent power-profiles-daemon
+rpm-ostree -q --peer uninstall --allow-inactive --idempotent firefox
+rpm-ostree -q --peer uninstall --allow-inactive --idempotent xwaylandvideobridge
 
 #System
 
 systemctl daemon-reload
 
-systemctl disable gdm
-
 systemctl mask systemd-rfkill systemd-rfkill.socket
-systemctl mask gdm
 
 systemctl enable tlp
 systemctl enable autopgrade.timer autopgrade
 systemctl enable boinc-client
 systemctl enable zram-init
 systemctl enable systemd-bsod
-systemctl enable sddm
+systemctl enable gdm
 
 plymouth-set-default-theme spinner
 
