2af0174d967a9422f03519d5327afb8762b6d581
Avaneesh <90239357+MrGrappleMan@users.noreply.github.com>
2025-03-10 18:51:30 +0530

    Update start.bash

--- start.bash
+++ start.bash
@@ -4,7 +4,6 @@
 rm -rf /tmp/Fynelium
 git clone https://github.com/MrGrappleMan/Fynelium.git /tmp/Fynelium
 chmod -R 755 /tmp/Fynelium
-cd /tmp/Fynelium
 
 # Prevent system power state change:
 systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target shutdown.target reboot.target poweroff.target halt.target
@@ -17,5 +16,4 @@ rpm-ostree -q --peer upgrade # Fix dependancies
 rpm-ostree apply-live --allow-replacement
 rpm-ostree -q --peer --allow-inactive --idempotent install fish
 rpm-ostree apply-live --allow-replacement
-curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 fish /tmp/Fynelium/main.fish
