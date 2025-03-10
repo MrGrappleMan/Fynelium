fc457bb27aea3617a8b32cf4cb81a075d52ff963
Avaneesh <90239357+MrGrappleMan@users.noreply.github.com>
2025-03-10 17:53:49 +0530

    Update main.fish

--- main.fish
+++ main.fish
@@ -10,5 +10,26 @@ cp -r LXroot/var/* /var/
 
 # System:-
 
+systemctl daemon-reload
+
+systemctl mask systemd-rfkill systemd-rfkill.socket
+
+systemctl enable tlp
+systemctl enable autopgrade.timer
+systemctl enable boinc-client
+systemctl enable zram-init
+systemctl enable systemd-bsod
+
+plymouth-set-default-theme spinner
+
+rqe kargs --append-if-missing=threadirqs
+rqe kargs --delete-if-present=rhgb
+rqe kargs --append-if-missing=sysrq_always_enabled=1
+rqe kargs --append-if-missing=consoleblank=1
+rqe kargs --append-if-missing=quiet
+rqe kargs --append-if-missing=loglevel=3
+rqe kargs --append-if-missing=preempt=full
+rqe initramfs --enable
+
 systemctl unmask hybrid-sleep.target shutdown.target reboot.target poweroff.target sleep.target
 systemctl reboot
