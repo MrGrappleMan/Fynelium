1e08139d8d5f8a0581ff61e9fbbab469c154513e
Avaneesh <90239357+MrGrappleMan@users.noreply.github.com>
2025-03-10 18:14:28 +0530

    Delete LXscripts/PKGREPO directory

--- LXscripts/PKGREPO/rpm-ostree.fish
+++
@@ -1,6 +0,0 @@
-#!/bin/fish
-source /etc/fish/config.fish
-fyn-functions
-
-rpm-ostree -q --peer rebase fedora:fedora/rawhide/aarch64/silverblue
-rpm-ostree -q --peer install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-rawhide.noarch.rpm
