0d3e9693ffea5cc49ede0b1cb84ad0cfbb278d64
Avaneesh <90239357+MrGrappleMan@users.noreply.github.com>
2025-03-13 03:19:56 +0530

    Update main.fish

--- main.fish
+++ main.fish
@@ -60,7 +60,7 @@ cp -r /tmp/Fynelium/LXroot/var/* /var/
 
  #RPM-OSTree
   #Repos
-   rpm-ostree -q --peer rebase fedora:fedora/rawhide/aarch64/silverblue
+   rpm-ostree -q --peer rebase fedora:fedora/rawhide/x86_64/silverblue
    rpm-ostree -q --peer install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-rawhide.noarch.rpm
    rpm-ostree -q --peer install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-rawhide.noarch.rpm
 
