f0c6f6849cbdbc0c6d6b08b078744672a3c1c244
Avaneesh <90239357+MrGrappleMan@users.noreply.github.com>
2025-03-13 03:10:04 +0530

    Update main.fish

--- main.fish
+++ main.fish
@@ -33,12 +33,12 @@ cp -r /tmp/Fynelium/LXroot/var/* /var/
    flatpak remote-add --if-not-exists --system dragon-nightly https://cdn.kde.org/flatpak/dragon-nightly/dragon-nightly.flatpakrepo
    flatpak remote-add --if-not-exists --system xwaylandvideobridge-nightly https://cdn.kde.org/flatpak/xwaylandvideobridge-nightly/xwaylandvideobridge-nightly.flatpakrepo
    flatpak remote-add --if-not-exists --system eos-apps https://ostree.endlessm.com/ostree/eos-apps
-flatpak remote-add --if-not-exists --system webkit https://software.igalia.com/flatpak-refs/webkit-sdk.flatpakrepo
-flatpak remote-add --if-not-exists --system flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
-flatpak remote-add --if-not-exists --system gnome-nightly https://nightly.gnome.org/gnome-nightly.flatpakrepo
-flatpak remote-add --if-not-exists --system webkit-sdk https://software.igalia.com/flatpak-refs/webkit-sdk.flatpakrepo
-flatpak remote-add --if-not-exists --system fedora oci+https://registry.fedoraproject.org
-flatpak remote-add --if-not-exists --system fedora-testing oci+https://registry.fedoraproject.org/#testing
+   flatpak remote-add --if-not-exists --system webkit https://software.igalia.com/flatpak-refs/webkit-sdk.flatpakrepo
+   flatpak remote-add --if-not-exists --system flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
+   flatpak remote-add --if-not-exists --system gnome-nightly https://nightly.gnome.org/gnome-nightly.flatpakrepo
+   flatpak remote-add --if-not-exists --system webkit-sdk https://software.igalia.com/flatpak-refs/webkit-sdk.flatpakrepo
+   flatpak remote-add --if-not-exists --system fedora oci+https://registry.fedoraproject.org
+   flatpak remote-add --if-not-exists --system fedora-testing oci+https://registry.fedoraproject.org/#testing
 flatpak remote-add --if-not-exists --system rhel https://flatpaks.redhat.io/rhel.flatpakrepo
 flatpak remote-add --if-not-exists --system eclipse-nightly https://download.eclipse.org/linuxtools/flatpak-I-builds/eclipse.flatpakrepo
 flatpak remote-add --if-not-exists --system elementaryos https://flatpak.elementary.io/repo.flatpakrepo
@@ -46,6 +46,7 @@ flatpak remote-add --if-not-exists --system pureos https://store.puri.sm/repo/st
 flatpak remote-add --if-not-exists --system kde-runtime-nightly https://cdn.kde.org/flatpak/kde-runtime-nightly/kde-runtime-nightly.flatpakrepo
 
 #Pkgs
+ #Add
  flatpak install --system --noninteractive --or-update flathub com.gopeed.Gopeed
  flatpak install --system --noninteractive --or-update flathub io.github.flattool.Warehouse
  flatpak install --system --noninteractive --or-update flathub com.github.rkoesters.xkcd-gtk
@@ -57,54 +58,53 @@ flatpak remote-add --if-not-exists --system kde-runtime-nightly https://cdn.kde.
  flatpak install --system --noninteractive --or-update flathub org.octave.Octave
 
 #RPM-OSTree
-#Repos
- rpm-ostree -q --peer rebase fedora:fedora/rawhide/aarch64/silverblue
- rpm-ostree -q --peer install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-rawhide.noarch.rpm
+ #Repos
+  rpm-ostree -q --peer rebase fedora:fedora/rawhide/aarch64/silverblue
+  rpm-ostree -q --peer install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-rawhide.noarch.rpm
+  rpm-ostree -q --peer install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-rawhide.noarch.rpm
 
-#Pkgs
-rpm-ostree -q --peer install --allow-inactive --idempotent tlp tlp-rdw
-rpm-ostree -q --peer install --allow-inactive --idempotent kde
-rpm-ostree -q --peer install --allow-inactive --idempotent kernel-modules-extra
-rpm-ostree -q --peer install --allow-inactive --idempotent dnf dnf-repo
-rpm-ostree -q --peer install --allow-inactive --idempotent ghostty
-rpm-ostree -q --peer install --allow-inactive --idempotent rustup rust
-rpm-ostree -q --peer install --allow-inactive --idempotent golang
-rpm-ostree -q --peer install --allow-inactive --idempotent distcc
-rpm-ostree -q --peer install --allow-inactive --idempotent ostree-devel 
-rpm-ostree -q --peer install --allow-inactive --idempotent zen-browser torbrowser-launcher
-rpm-ostree -q --peer install --allow-inactive --idempotent boinc-client
-rpm-ostree -q --peer install --allow-inactive --idempotent topgrade
-rpm-ostree -q --peer install --allow-inactive --idempotent gnome-software
-rpm-ostree -q --peer install --allow-inactive --idempotent flatseal flatpak-selinux flatpak-session-helper xdg-desktop-portal flatpak-libs libportal host-spawn
-rpm-ostree -q --peer install --allow-inactive --idempotent beep
+ #Pkgs
+  #Add
+   rpm-ostree -q --peer install --allow-inactive --idempotent tlp tlp-rdw
+   rpm-ostree -q --peer install --allow-inactive --idempotent kde
+   rpm-ostree -q --peer install --allow-inactive --idempotent kernel-modules-extra
+   rpm-ostree -q --peer install --allow-inactive --idempotent dnf dnf-repo
+  rpm-ostree -q --peer install --allow-inactive --idempotent ghostty
+  rpm-ostree -q --peer install --allow-inactive --idempotent rustup rust
+  rpm-ostree -q --peer install --allow-inactive --idempotent golang
+  rpm-ostree -q --peer install --allow-inactive --idempotent distcc
+  rpm-ostree -q --peer install --allow-inactive --idempotent ostree-devel 
+  rpm-ostree -q --peer install --allow-inactive --idempotent zen-browser torbrowser-launcher
+  rpm-ostree -q --peer install --allow-inactive --idempotent boinc-client
+  rpm-ostree -q --peer install --allow-inactive --idempotent topgrade
+  rpm-ostree -q --peer install --allow-inactive --idempotent gnome-software
+  rpm-ostree -q --peer install --allow-inactive --idempotent flatseal flatpak-selinux flatpak-session-helper xdg-desktop-portal flatpak-libs libportal host-spawn
+  rpm-ostree -q --peer install --allow-inactive --idempotent beep
 
-rpm-ostree -q --peer uninstall --allow-inactive --idempotent power-profiles-daemon
-rpm-ostree -q --peer uninstall --allow-inactive --idempotent firefox
-rpm-ostree -q --peer uninstall --allow-inactive --idempotent xwaylandvideobridge
-
+  rpm-ostree -q --peer uninstall --allow-inactive --idempotent power-profiles-daemon
+  rpm-ostree -q --peer uninstall --allow-inactive --idempotent firefox
+  rpm-ostree -q --peer uninstall --allow-inactive --idempotent xwaylandvideobridge
 #System
-
-systemctl daemon-reload
-
-systemctl mask systemd-rfkill systemd-rfkill.socket
-
-systemctl enable tlp
-systemctl enable autopgrade.timer autopgrade
-systemctl enable boinc-client
-systemctl enable zram-init
-systemctl enable systemd-bsod
-systemctl enable gdm
-
-plymouth-set-default-theme spinner
-
-rpm-ostree -q --peer kargs --append-if-missing=threadirqs
-rpm-ostree -q --peer kargs --delete-if-present=rhgb
-rpm-ostree -q --peer kargs --append-if-missing=sysrq_always_enabled=1
-rpm-ostree -q --peer kargs --append-if-missing=consoleblank=0
-rpm-ostree -q --peer kargs --append-if-missing=quiet
-rpm-ostree -q --peer kargs --append-if-missing=loglevel=3
-rpm-ostree -q --peer kargs --append-if-missing=preempt=full
-rpm-ostree -q initramfs --enable
-
-systemctl unmask hybrid-sleep.target shutdown.target reboot.target poweroff.target sleep.target
-systemctl reboot
+ #Reload
+  systemctl daemon-reload
+ #Mask
+  systemctl mask systemd-rfkill systemd-rfkill.socket
+ #Enable
+  systemctl enable tlp
+  systemctl enable autopgrade.timer autopgrade
+  systemctl enable boinc-client
+  systemctl enable zram-init
+  systemctl enable systemd-bsod
+  systemctl enable gdm
+ #KernelArgs
+  rpm-ostree -q --peer kargs --append-if-missing=threadirqs
+  rpm-ostree -q --peer kargs --delete-if-present=rhgb
+  rpm-ostree -q --peer kargs --append-if-missing=sysrq_always_enabled=1
+  rpm-ostree -q --peer kargs --append-if-missing=consoleblank=0
+  rpm-ostree -q --peer kargs --append-if-missing=quiet
+  rpm-ostree -q --peer kargs --append-if-missing=loglevel=3
+  rpm-ostree -q --peer kargs --append-if-missing=preempt=full
+  rpm-ostree -q initramfs --enable
+#Completion
+ systemctl unmask hybrid-sleep.target shutdown.target reboot.target poweroff.target sleep.target
+ systemctl reboot
