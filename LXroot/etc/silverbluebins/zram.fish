17533e46bd9c4a698263262bf69d5f97d45e0d9e
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2025-03-26 00:16:51 +0530

    Update and rename zram.fish to mem-mgr.fish

--- LXroot/etc/silverbluebins/zram.fish
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/fish
-sudo swapoff -a
-sudo rmmod zram
-sudo modprobe zram num_devices=1
-set -g tmem (awk '/^MemTotal:/ {print $2}' /proc/meminfo)
-echo $tmem | sudo tee /sys/block/zram0/disksize
-echo zstd | sudo tee /sys/block/zram0/comp_algorithm
-sudo mkswap /dev/zram0
-sudo swapon /dev/zram0
