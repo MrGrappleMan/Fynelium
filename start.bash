b6e1736aa1f943304765ba75b3c8e25bb7c6165a
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2025-03-27 19:43:30 +0530

    Update start.bash

--- start.bash
+++ start.bash
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 # Clone Repository
-rm -rf /tmp/Fynelium
-git clone https://github.com/MrGrappleMan/Fynelium.git /tmp/Fynelium
-chmod -R 777 /tmp/Fynelium/
+git clone https://github.com/MrGrappleMan/Fynelium.git /
 
 # Prevent system power state change:
 systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target shutdown.target reboot.target poweroff.target halt.target
@@ -15,4 +13,3 @@ rpm-ostree --peer upgrade
 rpm-ostree apply-live --allow-replacement
 rpm-ostree --peer --allow-inactive --idempotent install fish
 rpm-ostree apply-live --allow-replacement
-fish /tmp/Fynelium/main.fish
