4d3b57a631aa1307de5642e8fd263a3338b28342
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2025-03-31 00:53:10 +0530

    Delete LXroot directory

--- LXroot/etc/redneck-bin/refyne.fish
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/fish
-
-sudo rm -rf /tmp/Fynelium
-sudo git clone sudo https://github.com/MrGrappleMan/Fynelium.git /tmp/Fynelium
-sudo chmod -R 777 /tmp/Fynelium/
-sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target shutdown.target reboot.target poweroff.target halt.target
-sudo fish /tmp/Fynelium/main.fish
