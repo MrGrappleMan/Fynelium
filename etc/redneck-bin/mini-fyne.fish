563b807d8f4ffc05a7ef31b9aac3c4504360697c
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2025-04-15 14:48:42 +0530

    Update mini-fyne.fish

--- etc/redneck-bin/mini-fyne.fish
+++ etc/redneck-bin/mini-fyne.fish
@@ -4,8 +4,6 @@ if test (id -u) -ne 0
     exit
 end
 
-brh rebase unstable -y
 rpm-ostree reload -q --peer
 rpm-ostree upgrade -q --peer --allow-downgrade --bypass-driver
-rpm-ostree initramfs -q --peer --enable
 rpm-ostree apply-live --allow-replacement
