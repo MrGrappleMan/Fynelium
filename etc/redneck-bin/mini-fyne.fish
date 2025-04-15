11c9263ce6432a02a2e5777476c10921bacffaa5
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2025-04-15 14:42:02 +0530

    Update mini-fyne.fish

--- etc/redneck-bin/mini-fyne.fish
+++ etc/redneck-bin/mini-fyne.fish
@@ -1,3 +1,18 @@
 #!/usr/bin/fish
 
-run0 brh rebase unstable -y
+function check_error
+    if test $status -ne 0
+        echo "Error code: $status"
+        sleep 3
+        exit $status
+    end
+end
+
+if test (id -u) -ne 0
+    exit
+end
+
+brh rebase unstable -y
+rpm-ostree reload -q --peer
+rpm-ostree upgrade -q --peer --allow-downgrade --bypass-driver
+rpm-ostree initramfs -q --peer --enable
