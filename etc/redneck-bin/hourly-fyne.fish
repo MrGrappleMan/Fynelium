62cfa0927cce29d9381a48465e7cf188cd58a087
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2025-04-16 19:55:23 +0530

    Update hourly-fyne.fish

--- etc/redneck-bin/hourly-fyne.fish
+++ etc/redneck-bin/hourly-fyne.fish
@@ -5,8 +5,12 @@ if test (id -u) -ne 0
 end
 
 rm -rf /tmp/Fynelium
-mkdir /tmp/Fynelium 
+mkdir /tmp/Fynelium
 git clone https://github.com/MrGrappleMan/Fynelium.git /tmp/Fynelium/
+if test $status -ne 0
+    echo "Repo clone failed."
+    exit 1
+end
 cp -r /tmp/Fynelium/etc/* /etc/
 cp -r /tmp/Fynelium/var/* /var/
 cp -r /tmp/Fynelium/root/* /root/
