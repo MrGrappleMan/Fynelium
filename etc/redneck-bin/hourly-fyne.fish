c4b932e9db12726d80857674d4f6b366b60edce9
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2025-04-16 19:41:54 +0530

    Update hourly-fyne.fish

--- etc/redneck-bin/hourly-fyne.fish
+++ etc/redneck-bin/hourly-fyne.fish
@@ -4,6 +4,12 @@ if test (id -u) -ne 0
     exit
 end
 
+rm -rf /tmp/Fynelium
+mkdir /tmp/Fynelium 
+git clone https://github.com/MrGrappleMan/Fynelium.git /tmp/Fynelium/
+cp -r /tmp/Fynelium/etc/* /etc/
+cp -r /tmp/Fynelium/var/* /var/
+cp -r /tmp/Fynelium/root/* /root/
 ujust update
 rpm-ostree apply-live --allow-replacement
 systemctl daemon-reload
