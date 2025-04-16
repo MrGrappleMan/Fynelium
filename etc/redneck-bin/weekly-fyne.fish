3fb1e13ba34e4b0c1d77efa562e531aae37847fe
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2025-04-16 19:54:57 +0530

    Update weekly-fyne.fish

--- etc/redneck-bin/weekly-fyne.fish
+++ etc/redneck-bin/weekly-fyne.fish
@@ -4,13 +4,6 @@ if test (id -u) -ne 0
     exit
 end
 
-rm -rf /tmp/Fynelium
-mkdir /tmp/Fynelium 
-git clone https://github.com/MrGrappleMan/Fynelium.git /tmp/Fynelium/
-cp -r /tmp/Fynelium/etc/* /etc/
-cp -r /tmp/Fynelium/var/* /var/
-cp -r /tmp/Fynelium/root/* /root/
-
 #Flatpak
  #Repos
   flatpak remote-add --if-not-exists --system flathub https://flathub.org/repo/flathub.flatpakrepo
