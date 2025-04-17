cb144fb7bbfb7a4318e63eaab35e359257bd961d
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2025-04-18 00:58:32 +0530

    Update mem-mgr.fish

--- etc/redneck-bin/mem-mgr.fish
+++ etc/redneck-bin/mem-mgr.fish
@@ -1,10 +1,10 @@
 #!/usr/bin/fish
-sudo swapoff /dev/zram0
-sudo rmmod zram
-sudo modprobe zram num_devices=1
+swapoff /dev/zram0
+rmmod zram
+modprobe zram num_devices=1
 set -g m (awk '/^MemTotal:/ {print $2}' /proc/meminfo)
-echo $m | sudo tee /sys/block/zram0/disksize
-echo zstd | sudo tee /sys/block/zram0/comp_algorithm
-sudo chmod 600 /dev/zram0
-sudo mkswap /dev/zram0
-sudo swapon /dev/zram0 -p 10
+echo $m | tee /sys/block/zram0/disksize
+zstd | tee /sys/block/zram0/comp_algorithm
+chmod 600 /dev/zram0
+mkswap /dev/zram0
+swapon /dev/zram0 -p 32767
