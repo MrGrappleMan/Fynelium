adbec19038380242d09faf60923a08eefdc1de50
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2025-04-22 15:28:11 +0530

    Update refyne.fish

--- etc/redneck-bin/refyne.fish
+++ etc/redneck-bin/refyne.fish
@@ -10,6 +10,18 @@
     exit 1
  end
 
+#RepoClone
+ rm -rf /tmp/Fynelium
+ mkdir /tmp/Fynelium
+ git clone https://github.com/MrGrappleMan/Fynelium.git /tmp/Fynelium/
+ if test $status -ne 0
+  echo "Repo clone failed"
+  exit 1
+ end
+cp -r /tmp/Fynelium/etc/* /etc/
+cp -r /tmp/Fynelium/var/* /var/
+##cp -r /tmp/Fynelium/root/* /root/
+
 #RefreshX1
 rpm-ostree -q --peer reload
 rpm-ostree -q --peer upgrade --allow-downgrade
