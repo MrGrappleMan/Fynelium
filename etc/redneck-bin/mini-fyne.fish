2bca8b57bc916ead7ebad0f9bebc058202518e0a
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2025-05-09 16:16:52 +0530

    Update mini-fyne.fish

--- etc/redneck-bin/mini-fyne.fish
+++ etc/redneck-bin/mini-fyne.fish
@@ -1,3 +1,9 @@
 #!/bin/env /bin/fish
 
 nohup flatpak update --system -y --noninteractive
+
+rpm-ostree -q --peer reload
+rpm-ostree -q --peer cleanup -b
+
+rpm-ostree apply-live
+rpm-ostree apply-live --allow-replacement
