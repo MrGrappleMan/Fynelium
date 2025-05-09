d9bfe81ffd44246e0313e5dd281ed1238adbcd6a
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2025-05-09 16:18:36 +0530

    Update mini-fyne.fish

--- etc/redneck-bin/mini-fyne.fish
+++ etc/redneck-bin/mini-fyne.fish
@@ -4,6 +4,6 @@ nohup flatpak update --system -y --noninteractive
 
 rpm-ostree -q --peer reload
 rpm-ostree -q --peer cleanup -b
-
+rpm-ostree -q --peer upgrade --bypass-driver --allow-downgrade --trigger-automatic-update-policy
 rpm-ostree apply-live
 rpm-ostree apply-live --allow-replacement
