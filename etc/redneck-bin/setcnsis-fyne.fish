82d01f76fe8c33db6b7bafee7e4f0347748b0477
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2025-05-13 21:48:01 +0530

    Update setcnsis-fyne.fish

--- etc/redneck-bin/setcnsis-fyne.fish
+++ etc/redneck-bin/setcnsis-fyne.fish
@@ -185,11 +185,10 @@ for user_path in (ls -d /home/*)
   gsettings set org.gnome.desktop.break-reminders.eyesight delay-seconds 10;
   gsettings set org.gnome.desktop.break-reminders.eyesight duration-seconds 20;
   gsettings set org.gnome.desktop.remote-desktop.vnc enable false;
-  org.gnome.desktop.remote-desktop.rdp enable true;
-  org.gnome.desktop.remote-desktop.rdp negotiate-port true;
-  org.gnome.desktop.remote-desktop.rdp port 3389;
-  org.gnome.desktop.remote-desktop.rdp view-only false;
-  
+  gsettings set org.gnome.desktop.remote-desktop.rdp enable true;
+  gsettings set org.gnome.desktop.remote-desktop.rdp negotiate-port true;
+  gsettings set org.gnome.desktop.remote-desktop.rdp port 3389;
+  gsettings set org.gnome.desktop.remote-desktop.rdp view-only false;
  '
   runuser -l $username -c "fish -c '$user_commands'"
   echo ""
