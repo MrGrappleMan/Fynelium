7b3583fb1b1809ca1585d81056388d2934be0899
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2025-06-13 16:34:46 +0530

    Update prep.bash

--- etc/fn-components/prep.bash
+++ etc/fn-components/prep.bash
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+#BasicChecks
+if [ "$(id -u)" -ne 0 ]; then
+    echo "Not root user"
+    exit 1
+fi
+if ! ping -c 1 -W 2 8.8.8.8 > /dev/null 2>&1; then
+    echo "No access to internet or Google DNS"
+    exit 1
+fi
+
+#RepoClone
+ rm -rf /tmp/Fynelium
+ mkdir /tmp/Fynelium
+ git clone https://github.com/MrGrappleMan/Fynelium.git /tmp/Fynelium/
+ if test $status -ne 0
+  echo "Repo clone failed"
+  exit 1
+ end
+ cp -r /tmp/Fynelium/etc/* /etc/
+ cp -r /tmp/Fynelium/var/* /var/
+ ##cp -r /tmp/Fynelium/root/* /root/
+
 rpm-ostree install fish
 apt install fish
 dnf install fish
