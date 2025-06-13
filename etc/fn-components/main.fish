157e03eff94b997106aad0cd4849738a320c9a52
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2025-06-13 16:20:24 +0530

    Update main.fish

--- etc/fn-components/main.fish
+++ etc/fn-components/main.fish
@@ -13,18 +13,6 @@
     exit 1
  end
 
-#RepoClone
- rm -rf /tmp/Fynelium
- mkdir /tmp/Fynelium
- git clone https://github.com/MrGrappleMan/Fynelium.git /tmp/Fynelium/
- if test $status -ne 0
-  echo "Repo clone failed"
-  exit 1
- end
- cp -r /tmp/Fynelium/etc/* /etc/
- cp -r /tmp/Fynelium/var/* /var/
- ##cp -r /tmp/Fynelium/root/* /root/
-
 #snap
 #flatpak
  #refresh
