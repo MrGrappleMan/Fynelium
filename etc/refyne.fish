9c6311097eafdc7a1afbf4063d358abe98b74450
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2025-06-24 16:36:37 +0530

    Update refyne.fish

--- etc/refyne.fish
+++ etc/refyne.fish
@@ -8,10 +8,22 @@
   echo "Not root user"
   exit 1
  end
- if not ping -c 1 -W 2 1.1.1.1 > /dev/null
+ if not ping -c 1 -W 2 8.8.8.8 > /dev/null
     echo "No access to internet or Google DNS"
     exit 1
  end
+
+#RepoClone
+ rm -rf /tmp/Fynelium
+ mkdir /tmp/Fynelium
+ git clone https://github.com/MrGrappleMan/Fynelium.git /tmp/Fynelium/
+ if test $status -ne 0
+  echo "Repo clone failed"
+  exit 1
+ end
+ cp -r /tmp/Fynelium/etc/* /etc/
+ cp -r /tmp/Fynelium/var/* /var/
+ ##cp -r /tmp/Fynelium/root/* /root/
 
 #snap
 #flatpak
