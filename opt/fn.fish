56e43c625fbb4748feea2d833f36ec79d290c84c
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2025-07-12 18:11:36 +0530

    Update fn.fish

--- opt/fn.fish
+++ opt/fn.fish
@@ -168,7 +168,7 @@ end
   # systemctl disable \
   #  plymouth-halt plymouth-kexec plymouth-poweroff plymouth-quit-wait plymouth-quit plymouth-read-write plymouth-reboot plymouth-start plymouth-switch-root-initramfs plymouth-switch-root
   systemctl reenable \
-   systemd-resolved systemd-networkd systemd-networkd-wait-online systemd-timesyncd \
+   systemd-resolved systemd-networkd systemd-networkd-wait-online NetworkManager-wait-online systemd-timesyncd \
    boinc-client
    tlp \
    uupd.timer \
